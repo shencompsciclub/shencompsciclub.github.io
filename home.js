6d30c48532f7e4b4b43410295969f5b619886105
26nandtanu <150471303+26nandtanu@users.noreply.github.com>
2025-02-09 19:59:06 -0500

    edits

--- home.js
+++ home.js
@@ -1,3 +1,14 @@
+
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+  /* Open when someone clicks on the bars  */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
