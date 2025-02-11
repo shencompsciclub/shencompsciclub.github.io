c794f8e0e9514ee61d6aad80b5029bf140b40778
26nandtanu <150471303+26nandtanu@users.noreply.github.com>
2025-02-10 21:29:18 -0500

    updates

--- home.js
+++ home.js
@@ -1,10 +1,45 @@
-  /* Open when someone clicks on the bars  */
-function openNav(x) {
-  document.getElementById("myNav").style.width = "100%";
-  x.classList.toggle("change");
-}
+const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    modeToggle = document.querySelector(".dark-light"),
+    searchToggle = document.querySelector(".searchToggle"),
+    sidebarOpen = document.querySelector(".sidebarOpen"),
+    sidebarClose = document.querySelector(".sidebarClose");
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
+    let getMode = localStorage.getItem("mode");
+        if(getMode && getMode === "dark-mode"){
+            body.classList.toggle("dark");
+        }
+
+//code to toggle dark and light mode
+modeToggle.addEventListener("click", () => {
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
+
+    // js code to keep user on selected mode
+
+    if(!body.classList.contains("dark")){
+        localStorage.setItem("mode", "light-mode");
+    }else{
+        localStorage.setItem("mode", "dark-mode");
+    }
+});
+
+//code to toggle search box
+searchToggle.addEventListener("click", () => {
+    searchToggle.classList.toggle("active");
+});
+
+//code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+body.addEventListener("click", e => {
+        let clickedElm = e.target;
+
+    if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
+        nav.classList.remove("active");
+    }
+});
+
+
