f398950b5733ac95e162838049d11a9eed0809c0
JonahEMorgan <149741383+JonahEMorgan@users.noreply.github.com>
2025-02-24 07:56:44 -0500

    Comment schedule.js for Mr. Hanley

--- schedule.js
+++ schedule.js
@@ -1,24 +1,24 @@
 function monthNow() {
-	date = new Date();
-	return [date.getFullYear(), date.getMonth()];
+	date = new Date(); // Gets the time right now
+	return [date.getFullYear(), date.getMonth()]; // Splits it into month and year components
 }
 
-elytra.component("calendar", {
-	init(data) {
+elytra.component("calendar", { // Tells elytra.js that we want a custom element named calendar
+	init(data) { // Defines a function that handles creation of the custom element
 		var events = {};
-		for(var [date, label] of Object.entries(JSON.parse(data))) {
-			parts = date.split("/").map(Number);
-			events[new Date(2000 + parts[2], parts[0] - 1, parts[1])] = label;
+		for(var [date, label] of Object.entries(JSON.parse(data))) { // Loops through every event in the dates object
+			parts = date.split("/").map(Number); // Converts the date from a string to list
+			events[new Date(2000 + parts[2], parts[0] - 1, parts[1])] = label; // Constructs a date object and maps the event to it
 		}
-		var html = "<p>SUN</p><p>MON</p><p>TUE</p><p>WED</p><p>THU</p><p>FRI</p><p>SAT</p>";
-		var [year, month] = monthNow();
-		var first = new Date(year, month, 1).getDay();
-		var lastBefore = new Date(year, month, 0).getDate();
-		var last = new Date(year, month + 1, 0).getDate();
-		for(var i = first; i > 0; i--)
-			html += `<div class="old"><h3>${lastBefore - i}</h3><p>${events[new Date(year, month, -i)] ?? ""}</p></div>`;
-		for(var i = 1; i <= last; i++)
-			html += `<div><h3>${i}</h3><p>${events[new Date(year, month, i)] ?? ""}</p></div>`;
-		return html;
+		var html = "<p>SUN</p><p>MON</p><p>TUE</p><p>WED</p><p>THU</p><p>FRI</p><p>SAT</p>"; // These are the day headings
+		var [year, month] = monthNow(); // Gets both the current year and month
+		var first = new Date(year, month, 1).getDay(); // Gets the first day of the current month
+		var lastBefore = new Date(year, month, 0).getDate(); // Gets the day before the first day of the month or the last day of the previous month
+		var last = new Date(year, month + 1, 0).getDate(); // Gets the last day of the current month;
+		for(var i = first; i > 0; i--) // Loops through the excess days from the previous month
+			html += `<div class="old"><h3>${lastBefore - i}</h3><p>${events[new Date(year, month, -i)] ?? ""}</p></div>`; // Adds the events to the html using null coalescing operator
+		for(var i = 1; i <= last; i++) // Loops though the days of the current month
+			html += `<div><h3>${i}</h3><p>${events[new Date(year, month, i)] ?? ""}</p></div>`; // Adds the events to the html using null coalescing operator
+		return html; // Returns the html to elytra.js for creation of custom elements
 	}
 });
