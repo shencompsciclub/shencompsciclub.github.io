552e174ebb0597f2b5b45b1bf33a13d5cfe08555
JonahEMorgan <149741383+JonahEMorgan@users.noreply.github.com>
2025-03-07 20:14:08 -0500

    Merge both versions :)

--- schedule.js
+++ schedule.js
@@ -14,11 +14,15 @@ elytra.component("calendar", { // Tells elytra.js that we want a custom element 
 		var [year, month] = monthNow(); // Gets both the current year and month
 		var first = new Date(year, month, 1).getDay(); // Gets the first day of the current month
 		var lastBefore = new Date(year, month, 0).getDate(); // Gets the day before the first day of the month or the last day of the previous month
-		var last = new Date(year, month + 1, 0).getDate(); // Gets the last day of the current month;
+		var lastDate = new Date(year, month + 1, 0); // Gets the last day of the current month
+		var lastDay = lastDate.getDay(); // Gets the day of week
+		var last = lastDate.getDate(); // Get the day of month
 		for(var i = first; i > 0; i--) // Loops through the excess days from the previous month
-			html += `<div class="old"><h3>${lastBefore - i}</h3><p>${events[new Date(year, month, -i)] ?? ""}</p></div>`; // Adds the events to the html using null coalescing operator
+			html += `<div class="fade"><h3>${lastBefore - i}</h3><p>${events[new Date(year, month, -i)] ?? ""}</p></div>`; // Adds the events to the html using null coalescing operator
 		for(var i = 1; i <= last; i++) // Loops though the days of the current month
 			html += `<div><h3>${i}</h3><p>${events[new Date(year, month, i)] ?? ""}</p></div>`; // Adds the events to the html using null coalescing operator
+		for(var i = 1; i < 7 - lastDay; i++) // Loops through the excess days from the next month
+			html += `<div class="fade"><h3>${i}</h3><p>${events[new Date(year, month + 1, i)] ?? ""}</p></div>`; // Adds the events to the html using null coalescing operator
 		return html; // Returns the html to elytra.js for creation of custom elements
 	}
 });
